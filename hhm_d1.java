d9eb39586ef14d03a9de0ea95ca6312dfca8804a
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-04 00:53:14 +0530

    Done

---
+++ hhm_d1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class hhm_d1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Helo Jiii\n");
+        System.out.println("hello jii");
+
+        // to print a triangle
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+
+        // To print a triangle using single print statement
+
+        System.out.print("*\n**\n***\n****\n*****");
+    }
+}
