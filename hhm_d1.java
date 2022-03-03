9da71fcac0fb9c8575999304a5504ecef0193338
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-04 01:21:28 +0530

    Implemented the variableunderstanding function

--- hhm_d1.java
+++ hhm_d1.java
@@ -12,6 +12,30 @@ public class hhm_d1
         System.out.println(" *");
         System.out.println("*****");
     }
+
+    // To understand the variables
+    public static void understandVariables()
+    {
+        int x = 10;
+        int y = 30;
+
+        int sum = x + y;
+
+        System.out.println("Sum of " + x + " and " + y + " is " + sum);
+
+        int prd = x * y;
+        System.out.println("Product of " + x + " and " + y + " is " + prd);
+
+        int quo = x / y;
+        int quo_ = y / x;
+        int rem = y % x;
+        System.out.println(quo);
+        System.out.println(quo_);
+        System.out.println(rem);
+
+        int expression = (x * y) / (x + y);
+        System.out.println(expression);
+    }
     public static void main(String[] args)
     {
         // System.out.print("Helo Jiii\n");
@@ -29,6 +53,8 @@ public class hhm_d1
 
         //System.out.print("*\n**\n***\n****\n*****");
 
-        printZ();
+        //printZ();
+
+        understandVariables();
     }
 }
