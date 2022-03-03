6dd24e19d48a4503da8f77872ae415ba1584edf4
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-04 01:03:10 +0530

    Implemented the printZ function

--- hhm_d1.java
+++ hhm_d1.java
@@ -2,21 +2,33 @@ import java.util.*;
 
 public class hhm_d1
 {
+
+    // To print Z
+    public static void printZ()
+    {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
     public static void main(String[] args)
     {
-        System.out.print("Helo Jiii\n");
-        System.out.println("hello jii");
+        // System.out.print("Helo Jiii\n");
+        // System.out.println("hello jii");
 
         // to print a triangle
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        // System.out.println("*****");
 
 
         // To print a triangle using single print statement
 
-        System.out.print("*\n**\n***\n****\n*****");
+        //System.out.print("*\n**\n***\n****\n*****");
+
+        printZ();
     }
 }
