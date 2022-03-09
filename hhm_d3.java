327f513f29a25d3e0d9351773e40240c9fbf4e8c
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-09 23:22:35 +0530

    Implemented Understanding Input

---
+++ hhm_d3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class hhm_d3 {
+
+    public static void understandingInput()
+    {
+        Scanner scn = new Scanner(System.in);
+        // int n = scn.nextInt();
+
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+        // String name = scn.nextLine();
+
+        // System.out.println("Hello Jii " + name);
+
+       
+        String name = scn.nextLine();
+        int n = scn.nextInt();
+
+        System.out.println("Hello Jii " + name + " you entered " + n);
+    }
+    public static void main(String[] args)
+    {
+        understandingInput();
+
+    }
+    
+}
