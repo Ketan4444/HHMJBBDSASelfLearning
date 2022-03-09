2da461be9cb6cdb1555bd76cfa364e764d32e99a
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-10 01:00:46 +0530

    Implemented printFibonacci

--- hhm_d3.java
+++ hhm_d3.java
@@ -61,12 +61,27 @@ public class hhm_d3 {
             System.out.println(i);
         }
     }
+
+    public static void printFibonacci(int n)
+    {
+        int a = 0;
+        int b = 1;
+
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
     public static void main(String[] args)
     {
         Scanner scn = new Scanner(System.in);
         //understandingInput();
         //printPrime();
-        printPrimesInRange(scn.nextInt(), scn.nextInt());
+        //printPrimesInRange(scn.nextInt(), scn.nextInt());
+        printFibonacci(scn.nextInt());
 
     }
     
