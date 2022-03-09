60c021687de2699684c75a935cea8a97134ee959
Ketan4444 <ketanjuneja123@gmail.com>
2022-03-10 00:14:09 +0530

    Implemented isPrime

--- hhm_d3.java
+++ hhm_d3.java
@@ -21,9 +21,41 @@ public class hhm_d3 {
 
         System.out.println("Hello Jii " + name + " you entered " + n);
     }
+
+    public static boolean isPrime(int num)
+    {
+        boolean isPrime = true;
+
+        for(int div = 2; div * div <= num; div++)
+        {
+            if(num % div == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+
+    public static void printPrime()
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+
+        for(int i = 1 ; i <= t; i++)
+        {
+            int num = scn.nextInt();
+            if(isPrime(num))
+            System.out.println("prime");
+            else
+            System.out.println("not prime");
+        }
+    }
     public static void main(String[] args)
     {
-        understandingInput();
+        //understandingInput();
+        printPrime();
 
     }
     
